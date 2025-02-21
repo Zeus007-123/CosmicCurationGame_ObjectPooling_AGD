f628978ae4c248ebada353f4221041832f8a6ffc
AKSHAY R <akshay.renjith.1@gmail.com>
2025-02-22 01:44:24 +0530

    Created a GetBullet() method
    - Implemented a GetBullet() method and CreateNewPooledBullet() method in the bullet pool class.
    - Activated and positioned the bullet.
    - Updated PlayerController and PlayerService scripts.

--- Assets/Scripts/Bullets/BulletPool.cs
+++ Assets/Scripts/Bullets/BulletPool.cs
@@ -6,14 +6,36 @@ namespace CosmicCuration.Bullets
     {
         private BulletView bulletView;
         private BulletScriptableObject bulletScriptableObject;
-        private List<PooledBullets> pooledBullets = new List<PooledBullets>();
+        private List<PooledBullets> pooledBulletsList;
 
         public BulletPool(BulletView bulletView, BulletScriptableObject bulletScriptableObject) 
         {
             this.bulletView = bulletView;
             this.bulletScriptableObject = bulletScriptableObject;
+            this.pooledBulletsList = new List<PooledBullets>();
         }
+        public BulletController GetBullet()
+        {
+            if (pooledBulletsList.Count > 0) 
+            {
+                PooledBullets pooledBullets = pooledBulletsList.Find(item => !item.isUsed);
+                if (pooledBullets != null) 
+                { 
+                    pooledBullets.isUsed = true;
+                    return pooledBullets.Bullet;
+                }
+            }
+            return CreateNewPooledBullet();
+        }
+        private BulletController CreateNewPooledBullet()
+        {
+            PooledBullets pooledBullets = new PooledBullets();
+            pooledBullets.Bullet = new BulletController(bulletView, bulletScriptableObject);
+            pooledBullets.isUsed = true;
+            pooledBulletsList.Add(pooledBullets);
 
+            return pooledBullets.Bullet;
+        }
         public class PooledBullets
         {
             public BulletController Bullet;
