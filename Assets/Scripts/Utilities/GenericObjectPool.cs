7b21ef0140d9330cd1f8e4ac5a2cdc35f4b09204
AKSHAY R <akshay.renjith.1@gmail.com>
2025-02-22 22:02:23 +0530

    Implemented ReturnItem() method in Generic Object Pool
    - Implemented the ReturnItem() method to generic object pool class.

--- Assets/Scripts/Utilities/GenericObjectPool.cs
+++ Assets/Scripts/Utilities/GenericObjectPool.cs
@@ -33,6 +33,11 @@ namespace CosmicCuration.Utilities
         {
             throw new NotImplementedException("Child Class don't have implementation of CreateItem() ");
         }
+        public void ReturnItem(T item)
+        {
+            PooledItem<T> pooledItem = pooledItems.Find(i => i.Item.Equals(item));
+            pooledItem.isUsed = false;
+        }
         public class PooledItem<T>
         {
             public T Item;
