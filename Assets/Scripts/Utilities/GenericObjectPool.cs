cdf0494519fedfe68842fda06e532549ef4119ca
AKSHAY R <akshay.renjith.1@gmail.com>
2025-02-22 21:44:54 +0530

    Implemented GetItem() method in Generic Object Pool
    - Implemented a Generic Object Pool.
    - Implemented a generic object pool class that can handle objects of any type.
    - Implemented GetItem(), CreateNewPooledItem and CreateItem methods in generic object pool class.

---
+++ Assets/Scripts/Utilities/GenericObjectPool.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+
+namespace CosmicCuration.Utilities
+{
+    public class GenericObjectPool<T> where T : class
+    {
+        private List<PooledItem<T>> pooledItems = new List<PooledItem<T>>();
+
+        protected T GetItem()
+        {
+            if (pooledItems.Count > 0)
+            {
+                PooledItem<T> item = pooledItems.Find(item => !item.isUsed);
+                if (item != null)
+                {
+                    item.isUsed = true;
+                    return item.Item;
+                }
+            }
+            return CreateNewPooledItem();
+        }
+        protected T CreateNewPooledItem()
+        {
+            PooledItem<T> newItem = new PooledItem<T>();
+            newItem.Item = CreateItem();
+            newItem.isUsed = true;
+            pooledItems.Add(newItem);
+
+            return newItem.Item;
+        }
+        protected virtual T CreateItem()
+        {
+            throw new NotImplementedException("Child Class don't have implementation of CreateItem() ");
+        }
+        public class PooledItem<T>
+        {
+            public T Item;
+            public bool isUsed;
+        }
+    }
+}
